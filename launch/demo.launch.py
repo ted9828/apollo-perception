946ed0209ea9df31189ce4c53b0bcc0ed771df96
ted9828 <107598532+ted9828@users.noreply.github.com>
2025-09-10 09:48:20 +0900

    Update demo.launch.py

--- launch/demo.launch.py
+++ launch/demo.launch.py
@@ -1,1 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='apollo_perception',
+            executable='demo_node',
+            name='demo_node',
+            output='screen'
+        )
+    ])
 
